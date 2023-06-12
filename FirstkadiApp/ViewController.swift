179e993c4af874117875a40269c38ac255f6d14f
koki.suzuki <suzuki@hiramu.net>
2023-06-13 08:19:51 +0900

    Initial Commit

---
+++ FirstkadiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstkadiApp
+//
+//  Created by 鈴木光基 on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
